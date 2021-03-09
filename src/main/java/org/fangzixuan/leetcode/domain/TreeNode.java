c70cb95eb3a0078863c9a1077229f9ebe606cb43
fangzixuan <bebopfish@sina.com>
2021-03-10 02:21:57 +0800

    【bebopfish】完成"二叉树中的最大路径和"（困难）

---
+++ src/main/java/org/fangzixuan/leetcode/domain/TreeNode.java
@@ -0,0 +1,26 @@
+package org.fangzixuan.leetcode.domain;
+
+/**
+ * binary tree node with value
+ *
+ * @author BebopFish
+ * @since 2021/3/9
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
